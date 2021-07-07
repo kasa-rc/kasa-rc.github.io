8e9c6531e1ae1e0864b38a67d46120021dfc1b6d
kasa-rc <53846451+kasa-rc@users.noreply.github.com>
2021-07-07 14:20:53 -0500

    Add files via upload

--- alta_alumnos_KRC/alumnos.sql
+++ alta_alumnos_KRC/alumnos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-06-2021 a las 23:48:31
+-- Tiempo de generación: 07-07-2021 a las 21:19:49
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.1
 
@@ -31,15 +31,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `alta_alumnos` (
   `id` int(11) NOT NULL,
   `programa` varchar(50) NOT NULL,
-  `nombre` varchar(20) NOT NULL
+  `nombre` varchar(20) NOT NULL,
+  `apellidoP` varchar(20) NOT NULL,
+  `apellidoM` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `alta_alumnos`
 --
 
-INSERT INTO `alta_alumnos` (`id`, `programa`, `nombre`) VALUES
-(13, 'Ingeniería en Sistemas Computacionales', 'Curso de Android');
+INSERT INTO `alta_alumnos` (`id`, `programa`, `nombre`, `apellidoP`, `apellidoM`) VALUES
+(39, 'Ingeniería en Sistemas Computacionales', 'kkkkkkkkk', 'Kasandra', 'Kasandra'),
+(40, 'Ingeniería en Sistemas Computacionales', 'Kasandra', 'Kasandra', 'Kasandra'),
+(41, 'Ingeniería en Sistemas Computacionales', 'Kasandra', 'Kasandra', 'Kasandra'),
+(42, 'Ingeniería en Sistemas Computacionales', 'kkkkk', 'Curso de Android', 'Curso de Android'),
+(44, 'Ingeniería en Sistemas Computacionales', 'K', '<button type=\"button', '');
 
 --
 -- Índices para tablas volcadas
@@ -59,7 +65,7 @@ ALTER TABLE `alta_alumnos`
 -- AUTO_INCREMENT de la tabla `alta_alumnos`
 --
 ALTER TABLE `alta_alumnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
